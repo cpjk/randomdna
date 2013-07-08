99b5ab2f1b096556f374dfb4f7651b48c8aed735
Chris Kelly <cpjkelly@gmail.com>
2013-07-08 11:16:12 -0700

    If individual sequence length is less than the default line length (50), a newline is now correctly inserted before the

--- randomdna.py
+++ randomdna.py
@@ -6,6 +6,7 @@ import os
 import time
 import argparse
 
+
 parser = argparse.ArgumentParser()
 parser.add_argument("number_of_seqs", type=int,
                     help="the number of sequence files to be created")
@@ -19,15 +20,15 @@ lines_per_seq = int(args.length_of_seqs / chars_per_line)
 
 #last line may have less than 50 characters
 chars_in_last_line = int(args.length_of_seqs % chars_per_line)
-outputDir = os.path.join(os.curdir, 'output')
+outputdir = os.path.join(os.curdir, 'output')
 
-if not os.path.exists(outputDir):
-    os.makedirs(outputDir)
+if not os.path.exists(outputdir):
+    os.makedirs(outputdir)
 
 start_time = time.time()
-alphabet = ['a', 'c', 't', 'g']
+dna_alphabet = ['a', 'c', 't', 'g']
 filename = "sequences.fasta"
-seqfile = open(os.path.join(outputDir, filename), 'w')
+seqfile = open(os.path.join(outputdir, filename), 'w')
 
 for seq_number in seq_list:
     print "Writing sequence file number", seq_number,
@@ -37,15 +38,17 @@ for seq_number in seq_list:
 
     for line in range(1, lines_per_seq + 1):
         for index in range(1, chars_per_line + 1):
-           seqfile.write(random.choice(alphabet))
+           seqfile.write(random.choice(dna_alphabet))
         seqfile.write('\n')
 
     for index in range(1, chars_in_last_line + 1):
         # Write the last line in the sequence
-        seqfile.write(random.choice(alphabet))
+        seqfile.write(random.choice(dna_alphabet))
+
+    if lines_per_seq < 2:
+        seqfile.write('\n')
 
 run_time = time.time() - start_time
 
-print ('\b')
-print "\nDone"
-print("Execution time: %.2f seconds." % run_time)
+print "\b\nDone"
+print "Execution time: %.2f seconds." % run_time
